825f4b1adb9cac484d1741fce2ecd25f8d97845c
mihanick <mihanick@gmail.com>
2015-11-12 18:13:47 +0600

    Add code to create textboxes for each xml element

--- XmlEditor/XmlEditor/XmlEditorControl/Views/UserControl1.xaml.cs
+++ XmlEditor/XmlEditor/XmlEditorControl/Views/UserControl1.xaml.cs
@@ -16,6 +16,38 @@ namespace XmlEditor.XmlEditorControl.Views
             InitializeComponent();
             DataContext = new XmlFileViewModel();
 
+            // http://stackoverflow.com/questions/6250240/how-to-add-a-scrollbar-to-a-stackpanel
+            ScrollViewer sv = new ScrollViewer();
+            StackPanel sp = new StackPanel();
+            sv.Content = sp;
+            this.Content = sv;
+            foreach (XmlNode i in ((XmlFileViewModel)DataContext).XmlFile.xdoc.ChildNodes)
+            {
+                NestedControlPlacement(sp, i);
+            }
         }
 
+        private void NestedControlPlacement(StackPanel sp, XmlNode xn)
+        {
+            StackPanel spn = new StackPanel();
+            spn.Margin = new System.Windows.Thickness(4,0,0,0);
+            sp.Children.Add(spn);
+
+            if (xn.LocalName == "#text")
+            {
+                TextBox tb = new TextBox();
+                tb.Text = xn.Value;
+                spn.Children.Add(tb);
+            }
+            else
+            {
+                TextBlock blk = new TextBlock();
+                blk.Text = xn.Name;
+                spn.Children.Add(blk);
+
+                foreach (XmlNode xnd in xn.ChildNodes)
+                    NestedControlPlacement(spn, xnd);
+            }
+        }
+    }
 }
